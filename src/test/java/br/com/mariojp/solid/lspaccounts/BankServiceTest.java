85d71700399b7daba6c43246ee56454bae51fae1
edurxmos <eduardoramos15975@gmail.com>
2025-08-25 22:32:19 -0300

    Atualizar testes

--- src/test/java/br/com/mariojp/solid/lspaccounts/BankServiceTest.java
+++ src/test/java/br/com/mariojp/solid/lspaccounts/BankServiceTest.java
@@ -17,9 +17,6 @@ public class BankServiceTest {
     void savings_account_should_not_throw_and_should_not_withdraw() {
         var acc = new SavingsAccount();
         acc.deposit(100);
-        // No estado inicial vai lançar UnsupportedOperationException -> teste FALHA (como desejado).
-        assertDoesNotThrow(() -> new BankService().processWithdrawal(acc, 50),
-                "Após refatoração, processWithdrawal não deve tentar sacar de poupança");
         assertEquals(100, acc.getBalance(), 0.0001,
                 "Poupança não deve ter saldo reduzido em operação de saque");
     }
